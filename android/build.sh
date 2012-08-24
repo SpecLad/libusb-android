bc7e3d1fe8ee86ea947ec3459ca47074e2398b02
Roman Donchenko <roman.donchenko@itseez.com>
2012-08-24 15:41:46 +0400

    Added Android build scripts and thr Java helper class.

---
+++ android/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+dir=$(dirname "$0")
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 (armeabi | armeabi-v7a)"
+  exit 1
+fi
+
+build=$(sh $dir/../config.guess)
+
+echo "Guessed the build system as ${build}."
+
+cflags="-O2 -g"
+
+arch=$1
+
+case $arch in
+armeabi)
+  host=arm-linux-androideabi
+  cflags="$cflags -mthumb"
+  ldflags="-Wl,--fix-cortex-a8 -Wl,--no-undefined"
+  ;;
+armeabi-v7a)
+  host=arm-linux-androideabi
+  cflags="$cflags -mthumb -march=armv7-a -mfloat-abi=softfp"
+  ldflags="-Wl,--fix-cortex-a8 -Wl,--no-undefined"
+  ;;
+*)
+  echo "Unsupported architecture: $arch"
+  exit 1
+esac
+
+mkdir -p $dir/build-$arch
+(cd $dir/build-$arch && ../../configure --host=$host --build=$build CFLAGS="$cflags" LDFLAGS="$ldflags" && make)
+
+echo "Copying headers and libraries..."
+mkdir -p $dir/ndk-modules/libusb-1.0/{include/libusb-1.0,lib/$arch}
+cp $dir/../libusb/libusb.h $dir/ndk-modules/libusb-1.0/include/libusb-1.0
+cp $dir/build-$arch/libusb/.libs/libusb-1.0.so $dir/ndk-modules/libusb-1.0/lib/$arch
+
---
+++ android/ndk-modules/libusb-1.0/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libusb-1.0
+
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libusb-1.0.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/libusb-1.0
+include $(PREBUILT_SHARED_LIBRARY)
